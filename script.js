c57b9319db6c71d8037e077b251e4d8114fb7131
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-24 20:24:24 +0300

    todo-dom

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello");
+document.querySelector("#add").addEventListener("click",function(){
+    let todo = document.querySelector("#input").value
+    let ndiv =document.createElement("div")
+    ndiv.innerText = todo
+    ndiv.classList.add("task")
+    document.querySelector(".tasks").appendChild(ndiv)
+})
